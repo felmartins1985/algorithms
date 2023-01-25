e85985c13a8d33d7653ee8e9f5732cf012810209
hp <hp@pop-os.localdomain>
2023-01-25 16:02:09 -0300

    feat: requisito 2- testes

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("teste", 2) == "ets_et"
+    assert encrypt_message("teste2", 3) == "set_2et"
+    assert encrypt_message("teste2", 7) == "2etset"
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("teste", "teste")
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(1, 1)
