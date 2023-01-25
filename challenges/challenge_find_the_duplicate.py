839d55ed3c63407ede319eef7afb15e4d33cb974
hp <hp@pop-os.localdomain>
2023-01-25 18:46:44 -0300

    feat: requisito 5- numeros repetidos

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,15 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(nums) <= 1:
+        return False
+    nums.sort()
+    for i in range(len(nums) - 1):
+        if type(nums[i]) == str or nums[i] < 0:
+            return False
+        # if nums[i] < 0:
+        #     return False
+        if nums[i] == nums[i + 1]:
+            return nums[i]
+    return False
+
+
+print(find_duplicate(["guilherme", "felipe"]))
