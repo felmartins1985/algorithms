13de5fa2c25261167c9be6cdb8a6671d1bcc247b
hp <hp@pop-os.localdomain>
2023-01-25 15:19:50 -0300

    feat: requisito 1- study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    counter = 0
+    if target_time is None:
+        return None
+    for student in permanence_period:
+        if type(student[0]) != int or type(student[1]) != int:
+            return None
+        if student[0] <= target_time and student[1] >= target_time:
+            counter += 1
+
+    return counter
